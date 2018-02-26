a1ba7a2e0041eda54c6d2e9574da2bad2355d676
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-02-26 13:33:53 -0800

    coded a function to return the random question using the random generated number

--- baby.py
+++ baby.py
@@ -1,2 +1,14 @@
 import random
 
+def get_random_question():
+	questions = [
+		"Why is the sky blue?",
+		"Why do we have to go to school?",
+		"why do we have to sleep?"
+	]
+
+	rand_num = random.randint(0, len(questions))
+
+	return questions[rand_num]
+	
+
